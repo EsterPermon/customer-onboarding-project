b827b2e9bcd9fda082ce6091c058fb38b7211a0d
Ester Monteiro <esterpmonteiro@gmail.com>
2024-04-24 19:42:54 +0200

    Adding date validation

--- src/app/pages/next-steps/next-steps.component.ts
+++ src/app/pages/next-steps/next-steps.component.ts
@@ -16,16 +16,19 @@ export class NextStepsComponent implements OnInit {
   userInfoForm!: FormGroup;
   user$: Observable<User>;
   isFormInvalid: boolean;
+  isBirthDateInvalid: boolean;
   currentYear = new Date().getFullYear();
 
   constructor(private store: Store<AppState>) {
     this.user$ = this.store.select(selectUser);
     this.isFormInvalid = false;
+    this.isBirthDateInvalid = false;
   }
 
   ngOnInit(): void {
     this.user$.subscribe((user) => {
       this.isFormInvalid = false;
+      this.isBirthDateInvalid = false;
 
       this.userInfoForm = new FormGroup({
         gender: new FormControl(user.gender, Validators.required),
@@ -39,11 +42,24 @@ export class NextStepsComponent implements OnInit {
     });
   }
 
+  isValidDate(day: number, month: number, year: number) {
+    const birthDate = new Date(year, month - 1, day);
+    return birthDate.getMonth() === month - 1 && birthDate.getDate() === day;
+  }
+
   onSubmit() {
     if (this.userInfoForm.valid) {
       const userInfo = this.userInfoForm.value;
-      this.store.dispatch(addUser(userInfo));
-      localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      const day = userInfo.dayOfBirth;
+      const month = userInfo.monthOfBirth;
+      const year = userInfo.yearOfBirth;
+
+      this.isBirthDateInvalid = !this.isValidDate(day, month, year);
+
+      if (!this.isBirthDateInvalid) {
+        this.store.dispatch(addUser(userInfo));
+        localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      }
     } else {
       this.isFormInvalid = true;
     }
